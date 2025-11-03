8f2862e7fc53dd75a1918cd429623cad2ae83b33
Nguyen Anh Khoa <KhoaNASE182748@fpt.edu.vn>
2025-11-03 11:07:30 +0700

    T

--- Assets/Scripts/Tower/Tower.cs
+++ Assets/Scripts/Tower/Tower.cs
@@ -1,13 +1,50 @@
+
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Tower : MonoBehaviour
 {
     [SerializeField] private TowerData data;
+    private CircleCollider2D _circleCollider;
+
+    private List<Enemy> _enemiesInRange;
+
+    private void Start()
+    {
+        _circleCollider = GetComponent<CircleCollider2D>();
+        _circleCollider.radius = data.range;
+        _enemiesInRange = new List<Enemy>();
+    }
+
+
 
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.blue;
         Gizmos.DrawWireSphere(transform.position, data.range);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Enemy"))
+        {
+            Enemy enemy = collision.GetComponent<Enemy>();
+            //if (enemy != null && !_enemiesInRange.Contains(enemy))
+            //{
+            _enemiesInRange.Add(enemy);
+            //}
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Enemy"))
+        {
+            Enemy enemy = collision.GetComponent<Enemy>();
+            if (_enemiesInRange.Contains(enemy))
+            {
+                _enemiesInRange.Remove(enemy);
+            }
+        }
     }
 
 }
