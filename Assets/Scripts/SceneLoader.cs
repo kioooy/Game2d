28876072f8562ccd7832307e53a149b38f5fc24a
Nguyen Anh Khoa <KhoaNASE182748@fpt.edu.vn>
2025-11-29 12:49:04 +0700

    To Level Select

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -5,6 +5,6 @@ public class SceneLoader : MonoBehaviour
 {
     public void LoadScene()
     {
-        SceneManager.LoadScene("LevelSelect Map 1");
+        SceneManager.LoadScene("LevelSelect");
     }
 }
