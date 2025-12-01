2fc19b05a97812edaab3bce2e4025861f3238986
Nguyen Anh Khoa <KhoaNASE182748@fpt.edu.vn>
2025-12-01 15:44:41 +0700

    Try to press level tower button to go to the currently level XX

---
+++ Assets/Scripts/LevelSelector.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelector : MonoBehaviour
+
+{
+    public string level;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    public void OpenScene()
+    {
+        SceneManager.LoadScene("Level " + level.ToString());
+    }
+}
